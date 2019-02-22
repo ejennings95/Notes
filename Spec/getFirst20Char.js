8f28ccdca66ccc0657399075b7a834c2a4136f29
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-22 13:34:21 +0000

    test written for getting first 20 characters of a note

---
+++ Spec/getFirst20Char.js
@@ -0,0 +1,32 @@
+function testGetFirst20Char() {
+  function noteDouble(string) {
+    this._noteText = string;
+  }
+
+  noteDouble.prototype = {
+    showNote: function() {
+      return this._noteText;
+    }
+  }
+
+  function noteListDouble() {
+    this._notes = [];
+  }
+
+  noteListDouble.prototype = {
+    showNotes: function() {
+      return this._notes;
+    },
+    addNote: function(note) {
+      return this._notes.push(note);
+    },
+  }
+
+  var testNote = new noteDouble("how much of this can you see?");
+  var testNoteList = new noteListDouble();
+  var testNoteListView = new NoteListView(testNoteList);
+
+  testNoteList.addNote(testNote);
+  console.log("testGetFirst20Char: " + assert.isTrue(testNoteListView.getFirst20Char() === "how much of this can"  ));
+};
+testGetFirst20Char();
