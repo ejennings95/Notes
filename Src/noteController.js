d62205409fe37223a798d411cc33d9617ce28f05
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-22 13:25:53 +0000

    note controller to print note from notelist

--- Src/noteController.js
+++ Src/noteController.js
@@ -1,2 +1,12 @@
-var element = document.getElementById("app")
-element.innerHTML = "Howdy";
+(function (exports) {
+  function NoteController(noteList){
+    this._noteList = noteList;
+    this._view = new NoteListView(noteList);
+  }
+
+  NoteController.prototype.returnNotesInHTML = function () {
+    document.getElementById('app').innerHTML = this._view.printNotesInHTML()
+   };
+
+  exports.NoteController = NoteController;
+})(this);
