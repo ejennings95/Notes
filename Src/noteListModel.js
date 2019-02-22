d30c3882df988ac5cd05041333ef037d23205d25
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-22 13:04:12 +0000

    passed create note list model test

---
+++ Src/noteListModel.js
@@ -0,0 +1,16 @@
+(function (exports) {
+    function NoteList(){
+        this._notes = [];
+    }
+
+    NoteList.prototype.showNotes = function () {
+        return this._notes;
+    };
+
+    NoteList.prototype.addNote = function(note) {
+        return this._notes.push(note);
+    };
+
+    exports.NoteList = NoteList;
+
+})(this);
