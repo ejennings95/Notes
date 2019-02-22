76e4bd5c8c4ef85892d84b729fef3a74bd1f8fe4
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-22 13:42:09 +0000

    test written for print first 20 characters

---
+++ Spec/printFirst20Char.js
@@ -0,0 +1,32 @@
+function testPrintFirst20Char() {
+  function noteDouble(string) {
+    this._noteText = string;
+  }
+
+  noteDouble.prototype = {
+    showNote: function() {
+      return this._noteText;
+    }
+  }
+
+  function noteListDouble() {
+    this._notes = [];
+  }
+
+  noteListDouble.prototype = {
+    showNotes: function() {
+      return this._notes;
+    },
+    addNote: function(note) {
+      return this._notes.push(note);
+    },
+  }
+
+  var testNote = new noteDouble("how much of this can you see?");
+  var testNoteList = new noteListDouble();
+  var testNoteListView = new NoteListView(testNoteList);
+
+  testNoteList.addNote(testNote);
+  console.log("testPrintFirst20Char: " + assert.isTrue(testNoteListView.printFirst20Char() === "<li><div>how much of this can</div></li>"  ));
+};
+testPrintFirst20Char();
